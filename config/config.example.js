d0c3c1f2a7534632a492e2e17aa7e10b6495fd54
4013465w <4013465w@sina.com>
2016-11-22 13:02:29 +0800

    添加注释

--- config/config.example.js
+++ config/config.example.js
@@ -1,12 +1,12 @@
 module.exports = {
-  port: 3000,
-  root: '../',
-  user: '4013465w',
+  port: 3000,//此配置暂时无用，默认 9988
+  root: '../', //部署路径相对于部署程序的路径
+  user: '4013465w',//github 用户名，暂时仅支持 github
   mail:{
-    from:'503241187@qq.com',
-    pwd : '',
-    smtp : 'smtp.qq.com',
-    subject: '上线通知',
-    to: '503241187@qq.com'
+    from:'503241187@qq.com',//邮件发送邮箱
+    pwd : '',//发送邮箱密码
+    smtp : 'smtp.qq.com',//邮件服务器
+    subject: '上线通知',//邮件主题
+    to: '503241187@qq.com' //部署邮件接收邮箱
   }
 }
