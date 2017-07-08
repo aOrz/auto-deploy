978bb075d1483a5d18f7d48662220367b4527b76
aOrz <rememberwz@gmail.com>
2017-07-08 09:38:23 +0800

    tips

--- config/config.example.js
+++ config/config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000,//此配置暂时无用，默认 9988
+  port: 9988,//此配置暂时无用，默认 9988
   root: '../', //部署路径相对于部署程序的路径
   user: '4013465w',//github 用户名，暂时仅支持 github
   mail:{
@@ -9,8 +9,8 @@ module.exports = {
     subject: '上线通知',//邮件主题
     to: '503241187@qq.com' //部署邮件接收邮箱
   },
-  serverChan: {
+  serverChan: { //http://sc.ftqq.com/?c=code
     subject: '上线通知',//主题
-    sckey: 'SCU5965T351aefdf9da33c3debc59213a432ed5e58a464b74b733'
+    sckey: ''
   }
 }
