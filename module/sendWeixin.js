4136d0511960ea1ab725fd50e5a59538a70ea645
aOrz <rememberwz@gmail.com>
2017-10-22 14:15:01 +0800

    add imis

--- module/sendWeixin.js
+++ module/sendWeixin.js
@@ -1,38 +1,42 @@
 var http = require('http');
 
 var qs = require('querystring');
-var config = require('../config/config.js')
+var config = require('../config/config.js');
 
-module.exports = function (body, error) {
-    var data = {
-    text: config.serverChan.subject + (error ? "--失败" :''),
-    desp: body
-    };//这是需要提交的数据
+const Imis = require('imis-server-sdk');
+const imis = new Imis(config.serverChan.sckey, 'auto-deploy');
 
+module.exports = function(body, error) {
+  var data = {
+    text: config.serverChan.subject + (error ? '--失败' : ''),
+    desp: body,
+  }; //这是需要提交的数据
 
-var content = qs.stringify(data);
-    var options = {
-        hostname: 'sc.ftqq.com',
-        // port: 80,
-        path: '/' + config.serverChan.sckey + '.send?' + content    ,
-        method: 'GET'
-    };
+  imis.send({
+    logs: body,
+    notice: 0,
+    title: data.text,
+  });
+  var content = qs.stringify(data);
+  var options = {
+    hostname: 'sc.ftqq.com',
+    // port: 80,
+    path: '/' + config.serverChan.sckey + '.send?' + content,
+    method: 'GET',
+  };
 
-    var req = http.request(options, function (res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
+  var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      console.log('BODY: ' + chunk);
     });
+  });
 
-    req.on('error', function (e) {
-        console.log('problem with request: ' + e.message);
-    });
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
 
-    req.end();
-      
-}
-
-
+  req.end();
+};
